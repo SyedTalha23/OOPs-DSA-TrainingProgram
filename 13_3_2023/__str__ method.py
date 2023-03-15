a7217794a2388cae1f735d48bb0cef09712bc307
SyedTalha23 <127936259+SyedTalha23@users.noreply.github.com>
2023-03-15 14:40:56 +0530

    Delete 13_3_2023 directory

--- 13_3_2023/__str__ method.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    
-    def __str__(self):
-        return f"{self.name} is {self.age} years old"
-person = Person("John", 30)
-print(person)  # output: John is 30 years old
--- 13_3_2023/if else code.py
+++
@@ -1,9 +0,0 @@
-num=int(input("enter a number :"))
-if num%3==0 and num%3==0:
-    print("its multiple of both 3 and 5")
-elif num%3==0:
-    print("its a multiple of 3")
-elif num%5==0:
-    print("its a multiple of 5")
-else:
-    print("invalid input")
