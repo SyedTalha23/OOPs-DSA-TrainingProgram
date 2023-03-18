336f80b84e2b4b824863d7b6c1bc66b6145ea433
SyedTalha23 <127936259+SyedTalha23@users.noreply.github.com>
2023-03-18 19:32:50 +0530

    q bank customer.py

--- phase 1- python oop/18_3_2023/q bank customer.py
+++ phase 1- python oop/18_3_2023/q bank customer.py
@@ -1,3 +1,68 @@
+'''A bank has many customers and each customer has a bank account
+There are also privileged customers 
+who can earn bonus points for each of their transaction. 
+This scenario is depicted through the below class diagram.
+30 min
+
+Customer
+- customer_id
+- customer_name
+- age
+- account
+_init_(customer_id, customer_name, age, account)
++ withdraw(amount)
++ take_card()
++ get_customer_id()
++ get_customer_name()
++ get_age()
++ get_account()
+Account
+- account_type
+- balance
+- min_balance
+_init_(account_type,balance,min_balance)
++ get_account_type()
++ get_balance()
++ get_min_balance()
++ set_balance(balance)
+PrivilegedCustomer
+- bonus_points
+_init_ (customer_id, customer_name, age, account, 
+bonus_points)
++ withdraw(amount)
++ get_bonus_points()
+- increase_bonus()
+OverdrawException
+LimitReachedException
+
+RULES TO FOLLOW
+================
+Customer:
+withdraw(amount): This method should reduce the account balance based on the amount withdrawn. 
+Raise the following exceptions based on the given conditions.
+OverdrawException - If the amount to be withdrawn is greater than account balance.
+LimitReachedException - If the balance amount is less than minimum account balance.
+take_card(): Displays the message "Take card out from the ATM".
+PrivilegedCustomer:
+increase_bonus(): If the account balance is greater than 1000, increase the bonus points by 10, else increase it by 2.
+withdraw(amount): Invoke the parent class withdraw() method and increase the bonus points by calling increase_bonus() method, if no exceptions occured.
+If exceptions occur, display relevant messages. Ensure that the card is taken out from the ATM under any situation.
+
+Write a Python program to create a new PrivilegedCustomer with below details:
+Customer Id: 100
+Customer Name: Gopal
+Age: 43
+Bonus Points: 100
+Account Type: Savings
+Account Balance: 1000
+Account minimum: 500
+
+The customer should be able to withdraw money and also display the bonus points of the customer after the withdraw.'''
+
+
+
+
+
 class customer:
     def __init__(self,cid,cname,cage,account):
         self.customer_id=cid
